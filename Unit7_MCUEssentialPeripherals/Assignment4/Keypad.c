6bc439802b878984587f1c4266393498a1d24135
Mostafa Mahmoud Helmy <darshme7@gmail.com>
2021-06-02 19:00:52 +0200

    Unit 7 Section 3 Assignment 4

---
+++ Unit7_MCUEssentialPeripherals/Assignment4/Keypad.c
@@ -0,0 +1,115 @@
+/*
+ * Keypad.c
+ *
+ * Created: 28-May-21 5:51:18 PM
+ *  Author: Mostafa
+ */ 
+#include "Keypad.h"
+
+GPIO_config cfg;
+uint8 KEYPAD_row[]={R0,R1,R2,R3};
+uint8 KEYPAD_col[]={C0,C1,C2,C3};
+
+void KEYPAD_init(void){
+	uint8 i;
+	for(i=0;i<4;i++){
+		cfg.GPIO_PinNumber 	= KEYPAD_row[i];
+		cfg.GPIO_PinMode   	= GPIO_MODE_INPUT_PD;
+		cfg.GPIO_OutputSpeed	= GPIO_INPUT;
+		MCAL_GPIO_init(KEYPAD_Port, &cfg);
+		cfg.GPIO_PinNumber 	= KEYPAD_col[i];
+		cfg.GPIO_PinMode   	= GPIO_MODE_OUTPUT_PP;
+		cfg.GPIO_OutputSpeed	= GPIO_SPEED_10M;
+		MCAL_GPIO_init(KEYPAD_Port, &cfg);
+		MCAL_GPIO_writePin(KEYPAD_Port, KEYPAD_col[i], GPIO_LOW);
+	}
+}
+
+char KEYPAD_getChar(void){
+
+	uint8 i,j;
+	for(i=0;i<4;i++){
+		MCAL_GPIO_writePin(KEYPAD_Port, KEYPAD_col[i], GPIO_HIGH);
+		for(j=0;j<4;j++){
+			if(MCAL_GPIO_readPin(KEYPAD_Port, KEYPAD_row[j])){
+				while(MCAL_GPIO_readPin(KEYPAD_Port, KEYPAD_row[j]));
+				switch(i){
+				case 0:{
+					switch(j){
+					case 0:
+						return '7';
+						break;
+					case 1:
+						return '4';
+						break;
+					case 2:
+						return '1';
+						break;
+					case 3:
+						return '!';
+						break;
+					}
+					break;
+				}
+				case 1:{
+					switch(j){
+					case 0:
+						return '8';
+						break;
+					case 1:
+						return '5';
+						break;
+					case 2:
+						return '2';
+						break;
+					case 3:
+						return '0';
+						break;
+					}
+					break;
+				}
+				case 2:{
+					switch(j){
+					case 0:
+						return '9';
+						break;
+					case 1:
+						return '6';
+						break;
+					case 2:
+						return '3';
+						break;
+					case 3:
+						return '=';
+						break;
+
+					}
+					break;
+				}
+				case 3:{
+					switch(j){
+					case 0:
+						return '/';
+						break;
+					case 1:
+						return '*';
+						break;
+					case 2:
+						return '-';
+						break;
+					case 3:
+						return '+';
+						break;
+					}
+
+					break;
+				}
+				}
+
+			}
+		}
+		MCAL_GPIO_writePin(KEYPAD_Port, KEYPAD_col[i],GPIO_LOW);
+
+	}
+	return '\0';
+}																	
---
+++ Unit7_MCUEssentialPeripherals/Assignment4/LCD.h
@@ -0,0 +1,48 @@
+/*
+ * LCD.h
+ *
+ * Created: 27-May-21 7:53:05 PM
+ *  Author: Mostafa
+ */ 
+
+#include "stm32f103c6_gpio_driver.h"
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define RS		GPIO_PIN8
+#define RW		GPIO_PIN9
+#define EN		GPIO_PIN10
+
+#define BUSY 								1
+
+#define LCD_8BITS_2LINES_5X8			(0x38)
+#define LCD_4BITS_2LINES_5X8			(0x28)
+#define LCD_CLEAR_DISPLAY				(0x01)
+#define LCD_RETURN_HOME					(0x02)
+#define LCD_ENTRY_DEC					(0x04)
+#define LCD_ENTRY_INC					(0x06)
+#define LCD_ENTRY_DEC_SHIFT				(0x05)
+#define LCD_ENTRY_INC_SHIFT				(0x07)
+#define LCD_DISPLAY_OFF					(0x08)
+#define LCD_DISPLAY_ON					(0x0C)
+#define LCD_DISPLAY_ON_CURSOR			(0x0E)
+#define LCD_DISPLAY_ON_CURSOR_BLINK		(0x0F)
+#define LCD_DISPLAY_SHIFT_RIGHT			(0x1C)
+#define LCD_DISPLAY_SHIFT_LEFT			(0x18)
+#define LCD_CURSOR_SHIFT_RIGHT			(0x14)
+#define LCD_CURSOR_SHIFT_LEFT			(0x10)
+#define LCD_FIRST_LINE					(0x80)
+#define LCD_SECOND_LINE					(0xC0)
+
+#define LCD_control						GPIOA
+#define LCD_PORT						GPIOA
+
+void LCD_init(void);
+void LCD_printChar(char data);
+void LCD_printString(char * data);
+void LCD_goToXY(uint8 line ,uint8 position);
+void LCD_clearScreen(void);
+void delay(uint32 d);
+
+#endif /* LCD_H_ */
