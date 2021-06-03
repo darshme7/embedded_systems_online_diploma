873713229ffdd6c1e1e4dabf1c1e24d7708a59e7
Mostafa Mahmoud Helmy <darshme7@gmail.com>
2021-06-03 13:37:07 +0200

    Unit 7 Section 3 Assignment 4

--- Unit7_MCUEssentialPeripherals/Assignment4/7segment.h
+++ Unit7_MCUEssentialPeripherals/Assignment4/7segment.h
@@ -20,16 +20,16 @@
 #define _7SEGMENT_F 					GPIO_PIN14
 #define _7SEGMENT_G 					GPIO_PIN15
 
-#define ZERO 							0x01
-#define ONE 							0x79
-#define TWO 							0x24
-#define THREE 						0x30
-#define FOUR 							0x58
-#define FIVE 							0x12
-#define SIX 							0x02
-#define SEVEN							0x19
-#define EIGHT 						0x00
-#define NINE 							0x10
+#define ZERO 							    0x01
+#define ONE 							    0x79
+#define TWO 							    0x24
+#define THREE 						    0x30
+#define FOUR 							    0x58
+#define FIVE 							    0x12
+#define SIX 							    0x02
+#define SEVEN							    0x19
+#define EIGHT 						    0x00
+#define NINE 							    0x10
 
 void _7segment_init();
 void _7segment_printNumber(uint8 number);
