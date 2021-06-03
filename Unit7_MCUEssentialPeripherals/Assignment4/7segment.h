6644acd9b2abdd902244425f49e42086449b4378
Mostafa Mahmoud Helmy <darshme7@gmail.com>
2021-06-03 13:36:17 +0200

    Unit 7 Section 3 Assignment 4

--- Unit7_MCUEssentialPeripherals/Assignment4/7segment.h
+++ Unit7_MCUEssentialPeripherals/Assignment4/7segment.h
@@ -23,12 +23,12 @@
 #define ZERO 							0x01
 #define ONE 							0x79
 #define TWO 							0x24
-#define THREE 							0x30
+#define THREE 						0x30
 #define FOUR 							0x58
 #define FIVE 							0x12
 #define SIX 							0x02
 #define SEVEN							0x19
-#define EIGHT 							0x00
+#define EIGHT 						0x00
 #define NINE 							0x10
 
 void _7segment_init();
